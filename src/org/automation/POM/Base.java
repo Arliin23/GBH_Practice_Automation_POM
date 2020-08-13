33c99dfe9a960cc26189b32ac5057c3500d7519f
Arliin23 <arliin23@gmail.com>
2020-08-13 16:17:21 -0400

    Added the method to wait for a element to appear

--- src/org/automation/POM/Base.java
+++ src/org/automation/POM/Base.java
@@ -4,9 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     //Constructor
     public Base(WebDriver driver){
@@ -23,6 +26,18 @@ public class Base {
     //Method for wrap findElement
     public WebElement findElement(By locator){
         return driver.findElement(locator);
+    }
+
+    //Method for get Titles
+    public String getTitle(){
+    return driver.getTitle();
+    }
+
+
+    //Method for wait for a element to appear
+    public void waitElement(WebElement element){
+        wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 
 }

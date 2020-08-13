56790ea6fabafd2bf577141fb2501263f3be5df4
Arliin23 <arliin23@gmail.com>
2020-08-13 16:35:15 -0400

    Added open page and hover Method to the Base Class

--- src/org/automation/POM/Base.java
+++ src/org/automation/POM/Base.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -23,6 +24,16 @@ public class Base {
         return driver;
     }
 
+    //Method to open the page
+    public void visitPage(String URL){
+        driver.get(URL);
+    }
+
+    //Method for wrap Click
+    public void clickElement(By locator){
+        driver.findElement(locator).click();
+    }
+
     //Method for wrap findElement
     public WebElement findElement(By locator){
         return driver.findElement(locator);
@@ -33,11 +44,18 @@ public class Base {
     return driver.getTitle();
     }
 
-
-    //Method for wait for a element to appear
+    //Method to wait for a element to appear
     public void waitElement(WebElement element){
         wait = new WebDriverWait(driver,30);
         wait.until(ExpectedConditions.visibilityOf(element));
     }
+
+    //Method for hover
+    public void hoverLists(WebElement button){
+        //hover Action
+        Actions action = new Actions(driver);
+        action.moveToElement(button).perform();
+    }
+
 
 }
